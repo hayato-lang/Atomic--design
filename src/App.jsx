5470679aae3c0b40212d3ceacb11fb788ef271b6
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-22 17:53:20 +0900

    organism

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,18 @@ import './App.css';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
+import { UserCard } from './components/organisms/user/UserCard';
+
+const user = {
+  name: "勇人",
+  image: "https://source.unsplash.com/AOd8G5b2lzk",
+  email: "hayto@get.com",
+  phone: "090-1234-5678",
+  company: {
+    name: "ハゲタコ株式会社"
+  },
+  website: "https://google.com"
+};
 
 function App() {
   return (
@@ -10,6 +22,7 @@ function App() {
       <PrimaryButton>成功</PrimaryButton>
       <SecondaryButton>ハゲタコ</SecondaryButton>
       <SearchInput/>
+      <UserCard user={user}/>
     </div>
   );
 }
