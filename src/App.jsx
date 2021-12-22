b1148ee55baad8097cdaef84d4220d78af89d705
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-22 15:57:41 +0900

    moleculesを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css';
-import { PrimaryButton } from './components/atoms/PrimaryButton';
-import { SecondaryButton } from './components/atoms/SecondaryButton';
+import { PrimaryButton } from './components/atoms/button/PrimaryButton';
+import { SecondaryButton } from './components/atoms/button/SecondaryButton';
+import { SearchInput } from './components/molecules/SearchInput';
 
 function App() {
   return (
     <div className="App">
       <PrimaryButton>成功</PrimaryButton>
       <SecondaryButton>ハゲタコ</SecondaryButton>
+      <SearchInput/>
     </div>
   );
 }
